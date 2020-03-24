cd714f1b2b379ccf67c55dc5e67443f8ff8c983f
chic-caller-7520 <arbrown110@gmail.com>
2020-03-24 21:35:15 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,10 @@
-def introduction_with_language(name,language)
-  puts"Hi, my name is #{name} and I am learning to program in #{language}."
+ def introduction(name)
+  puts "Hi, my name is #{name}."
+end  
+
+ def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Sarah","Ruby")                                       
+
+introduction ("Sarah")
+introduction_with_language("Sarah", "Ruby")                                   
